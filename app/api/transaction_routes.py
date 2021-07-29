3532c4e344955dab5303e332143b916b55550b9b
Joubin T <jubin.taghdir@gmail.com>
2021-07-29 14:40:48 -0700

    finish send request backend

--- app/api/transaction_routes.py
+++ app/api/transaction_routes.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, jsonify, request
-from flask.wrappers import Request
 from flask_login import login_required
 from app.models import User, Transaction, db
-from app.forms import SignUpForm
+from app.forms import SendFundsForm
 from decimal import Decimal
 
 transaction_routes = Blueprint('transactions' ,__name__)
@@ -27,45 +26,40 @@ def get_transactions(id):
 
     transactions = Transaction.query.filter(Transaction.from_user_id == id).all()
 
-
     return {'transactions': [user_transaction.to_dict() for user_transaction in transactions]}
 
 
-@transaction_routes.route('/<int:id>/<tansaction_t>', methods=['POST'])
+@transaction_routes.route('/<int:id>/type/<filter_t>', methods=['POST'])
 # @login_required
-def post_transactions(id, tansaction_t):
+def post_transactions(id, filter_t):
     """
     creates a new transaction record
     """
+    transaction_type = filter_t
 
-    transaction_type = tansaction_t
+    form = SendFundsForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        if transaction_type == 'pay':
+            from_user_id = int(form.data['from_user_id'])
+            to_user_id = int(form.data['to_user_id'])
 
-    if transaction_type == 'pay':
-        from_user_id = int(request.json['from_user_id'])
-        to_user_id = int(request.json['to_user_id'])
-
-    elif transaction_type == 'request':
-        to_user_id = int(request.json['from_user_id'])
-        from_user_id = int(request.json['to_user_id'])
-
-        from_user_id = int(request.json['from_user_id'])
-        to_user_id = int(request.json['to_user_id'])
-    amount = Decimal(request.json['amount'])
-    transaction_status = 0 #0 : pending 1:accepted 2: rejected
-    crypto_type = request.json['crypto_type']
-
-    new_transaction = Transaction(
-        from_user_id,
-        to_user_id,
-        amount,
-        transaction_status,
-        crypto_type,
-    )
+        elif transaction_type == 'request':
+            to_user_id = int(form.data['from_user_id'])
+            from_user_id = int(form.data['to_user_id'])
     
-    db.session.add(new_transaction)
-    db.session.commit()
+        new_transaction = Transaction(
+            from_user_id,
+            to_user_id,
+            amount = Decimal(form.data['amount']),
+            transaction_status = 0, #0 : pending 1:accepted 2: rejected
+            crypto_type = form.data['crypto_type']
+        )
 
-    return {'transactions': [new_transaction.to_dict()]}
+        db.session.add(new_transaction)
+        db.session.commit()
+        return {'transactions': [new_transaction.to_dict()]}
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
 # @transaction_routes.route('/<int:id>/pay', methods=['PUT'])
--- app/forms/send_funds_form.py
+++ app/forms/send_funds_form.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField, DecimalField
 from wtforms.validators import DataRequired, Email, ValidationError
+from .signup_form import username_exists
 from app.models import Transaction
 
 
 class SendFundsForm(FlaskForm):
-    pass
+    from_user_id = StringField('from_user', validators=[DataRequired(), username_exists])
+    to_user_id = StringField('to_user', validators=[DataRequired(), username_exists])
+    amount = DecimalField('Amount', places=8, validators=[DataRequired()])
+    crypto_type = StringField('crypto_type', validators=[DataRequired()])

6178e70e11ec8d2de56255f180fb9299a2f4dc30
Joubin T <jubin.taghdir@gmail.com>
2021-08-20 10:51:25 -0700

    fix users file

--- react-app/src/components/UsersList.js
+++ react-app/src/components/UsersList.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { getUsers } from '../store/users';
 
 function UsersList() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('/api/users/');
-      const responseData = await response.json();
+      // const response = await fetch('/api/users/');
+      // const responseData = await response.json();
+      const responseData = await getUsers();
       setUsers(responseData.users);
     }
     fetchData();
+
   }, []);
 
   const userComponents = users.map((user) => {
